7f290cc727e45863fb2a4c718ee5ab25be0f78cf
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-05 23:05:16 +0530

    [Suraj]: Program for delegates demo is added.

---
+++ DelegatesDemo/DelegatesDemo/ArrayDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    internal class ArrayDelegates
+    {
+        delegate void DelegateOp(int a, int b);
+        public static void ImplementDelegate()
+        {
+            DelegateOp[] obj =
+            {
+                new DelegateOp(DelegatesDemo.Operations.Addition),
+                new DelegateOp(DelegatesDemo.Operations.Multiplication)
+            };
+            for (int i = 0; i < obj.Length; i++)
+            {
+                obj[i](2, 5);
+                obj[i](8, 9);
+                obj[i](4, 7);
+            }
+
+        }
+    }
+}
---
+++ DelegatesDemo/DelegatesDemo/Operations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    internal class Operations
+    {
+        public static void Addition(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("Sum of two no. is: "+ sum);
+        }
+        public static void Multiplication(int a, int b)
+        {
+            int mul = a * b;
+            Console.WriteLine("Multiplication of two no. is: " + mul);
+        }
+    }
+}
